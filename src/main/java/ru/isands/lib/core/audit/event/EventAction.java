2058cea1c81cbe360405a543c50ed08344c77b96
Akinin Igor <akinin@isand.ru>
2024-01-25 17:20:12 +0700

    add login name in event

--- src/main/java/ru/isands/lib/core/audit/event/EventAction.java
+++ src/main/java/ru/isands/lib/core/audit/event/EventAction.java
@@ -12,18 +12,18 @@ import java.util.List;
 public abstract class EventAction {
 
     private final AuditProperties auditProperties;
-    protected EventAction(AuditProperties auditProperties){
+
+    protected EventAction(AuditProperties auditProperties) {
         this.auditProperties = auditProperties;
     }
 
     @SneakyThrows
-    public Event createEvent(final String name, final List<EventParam> params)
-    {
+    public Event createEvent(final String name, final List<EventParam> params, String userName) {
         return Event.builder().module(auditProperties.getMetamodel().getModule())
                 .metamodelVersion(auditProperties.getMetamodel().getMetamodelVersion())
                 .name(name)
                 .userNode(InetAddress.getLocalHost().getHostAddress())
-                .userLogin("anonymous")
+                .userLogin(userName == null ? "anonymous" : userName)
                 .createdAt(new Timestamp(System.currentTimeMillis()).getTime())
                 .params(params)
                 .build();
